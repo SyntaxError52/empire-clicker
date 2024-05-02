13eaa00ccbace1d14a370dc9d957f5a3a7101dc2
cobblesteve <159058655+cobblesteve01@users.noreply.github.com>
2024-05-02 13:55:14 -0500

    add tons of console.log()

--- main.js
+++ main.js
@@ -4,6 +4,14 @@ let cps = 0;
 console.log('[Here to cheat arent you?]')
 console.log('[See code at https://github.com/SyntaxError52/empire-clicker.git]')
 
+console.log('[How to save]')
+console.log('[1. Right click and click Inspect]')
+console.log('[2. Go to console]')
+console.log('[3. Copy the code into the console: genSave()]')
+console.log('[4. If all is working the genSave function should generate code that you can use]')
+console.log('[5. Now if you reload you can load the save by using that code]')
+console.log('[Don\'t worry! A new save/load system is currently being worked on.]')
+
 function update(){
   document.getElementById('counter').innerHTML = `${cities} Cities, ${cps} Cps`;
 }
